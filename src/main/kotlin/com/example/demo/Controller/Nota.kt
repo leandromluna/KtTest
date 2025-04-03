bea21bae88f393312f2a389727fa87f2b2793cf7
pachi <leandro.luna25@hotmail.com>
2025-04-03 12:13:37 -0300

    add constructor n init

--- src/main/kotlin/com/example/demo/Controller/Nota.kt
+++ src/main/kotlin/com/example/demo/Controller/Nota.kt
@@ -2,8 +2,12 @@ package com.example.demo.Controller
 
 import java.util.*
 
-class Nota {
-    var nombre: String = ""
-    var contenido: String = ""
-    var fechaCreacion: Date = Date()
+class Nota (var nombre: String, var contenido: String, var fechaCreacion: Date) {
+
+    init {
+        nombre = nombre.uppercase()
+    }
+    constructor(nombre:String, contenido:String) : this (nombre, contenido, Date()){
+        println("Se instanceo la clase con el constructor secundario")
+    }
 }
--- src/main/kotlin/com/example/demo/DemoApplication.kt
+++ src/main/kotlin/com/example/demo/DemoApplication.kt
@@ -4,6 +4,7 @@ import com.example.demo.Controller.Controller
 import com.example.demo.Controller.Nota
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import java.util.*
 
 @SpringBootApplication
 class DemoApplication
@@ -12,8 +13,8 @@ fun main(args: Array<String>) {
 
     runApplication<DemoApplication>(*args)
     val c = Controller()
-    val nota1 = Nota()
-    val nota2 = Nota()
+    val nota1 = Nota("Nota1", "Contenido1")
+    val nota2 = Nota("Nota2", "Contenido2", Date())
     //c.mostrameMensaje();
     //c.mostrarFecha();
     //c.ejemplo("Auto", 200);
@@ -25,16 +26,14 @@ fun main(args: Array<String>) {
     val empleados = arrayListOf("Jesus", "Jose", "Gabriel")
     //c.printNombres(empleados);
     //print(c.edadUsuario(10))
-    nota1.nombre = "Nota Actualizada"
-    nota1.contenido = "Contenido"
+
 //    println("""
 //        *** Nota ***
 //        Nombre: ${nota1.nombre}
 //        Contenido: ${nota1.contenido}
 //        Fecha: ${nota1.fechaCreacion}
 //    """.trimIndent())
-    nota2.nombre = "Hola"
-    nota2.contenido = "ejemplo"
+
     val listaNotas = arrayListOf<Nota>()
     listaNotas.add(nota1)
     listaNotas.add(nota2)
