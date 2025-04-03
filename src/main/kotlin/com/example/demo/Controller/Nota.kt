c0ea92989a7cfc1156aca8397239f6e135a072cb
pachi <leandro.luna25@hotmail.com>
2025-04-03 11:35:25 -0300

    add nota.kt

---
+++ src/main/kotlin/com/example/demo/Controller/Nota.kt
@@ -0,0 +1,9 @@
+package com.example.demo.Controller
+
+import java.util.*
+
+class Nota {
+    var nombre: String = ""
+    var contenido: String = ""
+    var fechaCreacion: Date = Date()
+}
--- src/main/kotlin/com/example/demo/DemoApplication.kt
+++ src/main/kotlin/com/example/demo/DemoApplication.kt
@@ -1,6 +1,7 @@
 package com.example.demo
 
 import com.example.demo.Controller.Controller
+import com.example.demo.Controller.Nota
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
@@ -11,6 +12,8 @@ fun main(args: Array<String>) {
 
     runApplication<DemoApplication>(*args)
     val c = Controller()
+    val nota1 = Nota()
+    val nota2 = Nota()
     //c.mostrameMensaje();
     //c.mostrarFecha();
     //c.ejemplo("Auto", 200);
@@ -21,5 +24,26 @@ fun main(args: Array<String>) {
     //print(c.edadUsuario(4412))
     val empleados = arrayListOf("Jesus", "Jose", "Gabriel")
     //c.printNombres(empleados);
-    print(c.edadUsuario(10))
+    //print(c.edadUsuario(10))
+    nota1.nombre = "Nota Actualizada"
+    nota1.contenido = "Contenido"
+//    println("""
+//        *** Nota ***
+//        Nombre: ${nota1.nombre}
+//        Contenido: ${nota1.contenido}
+//        Fecha: ${nota1.fechaCreacion}
+//    """.trimIndent())
+    nota2.nombre = "Hola"
+    nota2.contenido = "ejemplo"
+    val listaNotas = arrayListOf<Nota>()
+    listaNotas.add(nota1)
+    listaNotas.add(nota2)
+    listaNotas.forEach{
+        println("""
+            ${it.nombre}
+            ${it.contenido}
+            ${it.fechaCreacion}
+            --------------------
+        """.trimIndent())
+    }
 }
